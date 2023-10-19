e985ba9f1203425675ed27aaa83c518083c036f4
Сайлаубеков Талгат Ерханулы <SaylaubekovT@lcoast.itstep>
2023-10-19 15:55:45 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data: () => {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+};
+</script>
+
+<template>
+  <div>App</div>
+</template>
+
+<style scoped></style>
