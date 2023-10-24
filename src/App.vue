16cc825b1b5d81abffaf15d8b15b239d527d1c35
Сайлаубеков Талгат Ерханулы <SaylaubekovT@lcoast.itstep>
2023-10-24 17:09:09 +0600

    запросы

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <script>
+import axios from "axios";
 export default {
   data: () => {
-    return {};
+    return {
+      info: null,
+      EURInfo: null,
+      USDInfo: null,
+      GBPInfo: null,
+    };
   },
-  computed: {},
-  watch: {},
-  methods: {},
+  watch: {
+    info() {
+      this.EURInfo = this.info.EUR;
+      this.USDInfo = this.info.USD;
+      this.GBPInfo = this.info.GBP;
+      console.log(this.EURInfo);
+    },
+  },
+  mounted() {
+    axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then((response) => response.data)
+      .then((res) => {
+        this.info = res.bpi;
+        console.log(this.info);
+      });
+  },
 };
 </script>
 
 <template>
-  <div class="app">App</div>
+  <div class="card">{{ info }}</div>
 </template>
 
 <style scoped></style>
